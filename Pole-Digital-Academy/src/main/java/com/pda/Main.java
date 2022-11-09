2796ce4f06c77a71783620a89a66c05c2d04b85d
touzaelhassan <touzaelhassan@gmail.com>
2022-11-09 20:54:06 +0100

    update

--- Pole-Digital-Academy/src/main/java/com/pda/Main.java
+++ Pole-Digital-Academy/src/main/java/com/pda/Main.java
@@ -1,13 +1,15 @@
 package com.pda;
 
+import com.pda.utils.PersistenceManager;
+
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 
 public class Main {
+
     public static void main(String[] args) {
 
-
+        EntityManager em = PersistenceManager.getEntityManager();
 
     }
+
 }
