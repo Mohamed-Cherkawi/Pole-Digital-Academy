62c78ae3fa61441168c367ff45cb70e8e4e21b63
touzaelhassan <touzaelhassan@gmail.com>
2022-11-09 20:38:46 +0100

    Creating Persistence Manager class for managing database connection using singleton pattern

--- Pole-Digital-Academy/src/main/java/com/pda/Main.java
+++ Pole-Digital-Academy/src/main/java/com/pda/Main.java
@@ -6,7 +6,8 @@ import jakarta.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-            Persistence.createEntityManagerFactory("pol");
-          // EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+
     }
 }
